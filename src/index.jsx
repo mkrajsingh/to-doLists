f49672d3059ac9c3990cffd8b09a617825812eef
Mithilesh kumar <149149533+mkrajsingh@users.noreply.github.com>
2025-03-22 20:54:32 +0530

    index.jsx

--- src/index.jsx
+++ src/index.jsx
@@ -1,8 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
+import { rootElement } from "./rootElement.jsx";
 
-const rootElement = document.getElementById("root")!;
 const root = ReactDOM.createRoot(rootElement);
 
 root.render(
